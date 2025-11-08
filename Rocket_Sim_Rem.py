df192d0da60826131af2af1698b69998f6974bd0
0zacharypackard3-jpg <0zacharypackard3@gmail.com>
2025-11-08 18:56:53 +0000

    Plot_Two

--- Rocket_Sim_Rem.py
+++ Rocket_Sim_Rem.py
@@ -6,35 +6,33 @@ import matplotlib.pyplot as plt
 
 
 #Begin with the constants 
-gravity = 9.8 #Gravity 
+gravity = 9.8 
 dt = 0.1
 
 # Time for Thrust
-srb = 7_500_000 - 1_200_000 #Solid Rocket Booster in KG aswell
+srb = 7_500_000 - 1_200_000 
 thrust_main = srb
-ssme = 375_000 # Space Shuttle Main Engines also in KG
+ssme = 375_000 
 thrust_minor = 3 * ssme
-net_thrust = thrust_main + thrust_minor # #Up is good thrust make go up
-burn_time = 200 # not sure what to put here yet. Lets assume 200 seconds for now
-sim_time = 300 # has to be larger than burn_time because we dont want a instant velocity.
+net_thrust = thrust_main + thrust_minor 
+burn_time = 200 
+sim_time = 300 
 time = np.arange(0, sim_time +dt, dt)
 
-# Overall distance should be D= A*T where a is constant acceleration and t is just time.
-acceleration = net_thrust * time # overall positive forces mulitplied by the time they are acting
+#Velocity
+acceleration = net_thrust * time
 distance =  0.5 * acceleration * time**2
 velocity = distance/time
-V = velocity # V is total average velocity which is described as distance / time
+V = velocity
 
-
-#Drag is described by D = cd * (I think) (rho V^2)/2 * A
+#Drag D = .5 * cd *(rho V^2)/2 * A
 cd = 0.78 # @ 40 degrees According to Reddit
 rho = 1.225 #kg/m^3
 A = 250 # m^2 the reference area of the shuttle was 2690 ft^2 = 250m^2
 drag = 0.5 * cd * rho * V**2 / 2 * A
 
-#L= 21CLρvA
 
-#as such we have to move onto lift.
+#lift= 21CLρvA
 q = rho
 S = A
 cL = 1.2
@@ -42,28 +40,17 @@ lift = cL * (rho * V**2)/2 * A
 # You need to further look at this its not good at all tbh. But it works for a place holder.
 
 
-mass = 4_470_000 #lbs approximate weight of the filled space shuttle
-weight = mass * gravity  #Weight is the force of gravity applied to the mass of an object 
-#Also weight will be represented in newtons (N) , while mass will be represented in kilograms (KG) because science is good,
-# I am exhausted I believe most of the calculations are a good starting point but I have class
+mass = (4_470_000)*2.2 #lbs times 2.2 equals kg
+weight = mass * gravity  
 
 #Time to make the rocket actually move
 up_force = distance + lift
 down_force = weight + drag
 height = up_force - down_force
 
-# Also github has both been great and terrible to use at the same time. I hope I dont have to spend another hour fixing the codespace tommorow.
-#--------------------------------------------------------------------------
-#I am going to move onto the plotting code now.
-
-
 plt.figure(figsize =(12, 8)) #sizing of the figure
-
-#define the variables
 plt.plot(time, velocity, label="Velocity (m/s)")
 plt.plot(time, height, label="Height (m)")
-
-#Define the Axis
 plt.xlabel("Time(s)")
 plt.ylabel("Height")
 plt.title("Space Shuttle Simulation")
@@ -72,4 +59,13 @@ plt.grid(True)
 plt.show()
 
 
-##OKie dokie got that part done
+
+
+#The height is not realistic in my opinion as such I need to rewrite the upwards forces
+#The overall code structure looks terrible, rewrite.
+#But it did produce a working simulation even if entirely unrealistic and bad.
+#Tommorow the improvements will be made
+
+#Notes to self plot works fine, maybe add some color codes and another y axis to describe upwards force because I want to have a second stage to the burn_time
+#After fix velocity  up_force and time during the plot.
+#I think lift drag and weight are good though.
