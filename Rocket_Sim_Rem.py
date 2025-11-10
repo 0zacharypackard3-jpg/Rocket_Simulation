d17ca281d64d63b8a8a6747fb77505361b5a7d09
0zacharypackard3-jpg <0zacharypackard3@gmail.com>
2025-11-10 17:33:37 +0000

    Update_Plot, physics

--- Rocket_Sim_Rem.py
+++ Rocket_Sim_Rem.py
@@ -5,67 +5,61 @@ import scipy as sp
 import matplotlib.pyplot as plt
 
 
-#Begin with the constants 
-gravity = 9.8 
 dt = 0.1
+sim_time = 150
+time = np.arange(0, sim_time + dt ,dt)
 
-# Time for Thrust
-srb = 7_500_000 - 1_200_000 
-thrust_main = srb
-ssme = 375_000 
-thrust_minor = 3 * ssme
-net_thrust = thrust_main + thrust_minor 
-burn_time = 200 
-sim_time = 300 
-time = np.arange(0, sim_time +dt, dt)
+#Earth mass
+earth_radius = 3963.19*1609.34  
+mass_earth = 5.972 *10**24 
+G = 6.674 * 10**-11 
+g = (G * mass_earth) / (earth_radius)**2
 
-#Velocity
-acceleration = net_thrust * time
-distance =  0.5 * acceleration * time**2
-velocity = distance/time
-V = velocity
+rocket_mass = 199
+upwards_force = 1959.655
 
-#Drag D = .5 * cd *(rho V^2)/2 * A
-cd = 0.78 # @ 40 degrees According to Reddit
-rho = 1.225 #kg/m^3
-A = 250 # m^2 the reference area of the shuttle was 2690 ft^2 = 250m^2
-drag = 0.5 * cd * rho * V**2 / 2 * A
+height = 0
+velocity = 0
+
+velocity_list = []
+height_list = []
 
 
-#lift= 21CLρvA
-q = rho
-S = A
-cL = 1.2
-lift = cL * (rho * V**2)/2 * A
-# You need to further look at this its not good at all tbh. But it works for a place holder.
+for t in time:
+    g = (G * mass_earth) / (earth_radius + height)**2
+    delta_g = g - G * mass_earth / earth_radius**2
+    rocket_weight = rocket_mass * g
+    acceleration = (upwards_force - rocket_weight) / rocket_mass
+    velocity += acceleration * dt
+    height += velocity * dt
+    velocity_list.append(velocity)
+    height_list.append(height)
 
 
-mass = (4_470_000)*2.2 #lbs times 2.2 equals kg
-weight = mass * gravity  
 
-#Time to make the rocket actually move
-up_force = distance + lift
-down_force = weight + drag
-height = up_force - down_force
+print()
+force_required = rocket_mass * g
 
-plt.figure(figsize =(12, 8)) #sizing of the figure
-plt.plot(time, velocity, label="Velocity (m/s)")
-plt.plot(time, height, label="Height (m)")
-plt.xlabel("Time(s)")
-plt.ylabel("Height")
-plt.title("Space Shuttle Simulation")
-plt.legend
-plt.grid(True)
+print(f"Required force for liftoff (N):", force_required )
+print("height(m): ",height)
+
+print()
+
+fig, ax1 = plt.subplots(figsize=(12, 8))
+
+ax1.plot(time, height_list, color="blue", label="Height (m)")
+ax1.set_xlabel("Time (s)")
+ax1.set_ylabel("Height (m)", color="blue")
+ax1.tick_params(axis='y', labelcolor="blue")
+ax1.grid(True)
+
+ax2 = ax1.twinx()
+ax2.plot(time, velocity_list, color="red", label="Velocity (m/s)")
+ax2.set_ylabel("Velocity (m/s)", color="red")
+ax2.tick_params(axis='y', labelcolor="red")
+
+fig.tight_layout()
+ax1.legend(loc="upper left")
+ax2.legend(loc="upper right")
+plt.title("Rocket_Simulation")
 plt.show()
-
-
-
-
-#The height is not realistic in my opinion as such I need to rewrite the upwards forces
-#The overall code structure looks terrible, rewrite.
-#But it did produce a working simulation even if entirely unrealistic and bad.
-#Tommorow the improvements will be made
-
-#Notes to self plot works fine, maybe add some color codes and another y axis to describe upwards force because I want to have a second stage to the burn_time
-#After fix velocity  up_force and time during the plot.
-#I think lift drag and weight are good though.
